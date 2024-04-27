8ef207b17e748cc5c42befc47c0f89149d43d3a3
ichiragkumar <chiragroy3030@gmail.com>
2024-04-27 20:13:32 +0530

    info can store in firestore

--- app/src/main/java/com/example/quotesapp/EditProfileScreen.kt
+++ app/src/main/java/com/example/quotesapp/EditProfileScreen.kt
@@ -1,114 +1,157 @@
-package com.example.quotesapp
+    package com.example.quotesapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
-import android.app.DatePickerDialog
-import android.icu.util.Calendar
-import android.view.View
-import com.google.firebase.database.FirebaseDatabase
+    import android.content.Intent
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.widget.ArrayAdapter
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.ImageButton
+    import android.widget.Spinner
+    import com.google.firebase.auth.FirebaseUser
+    import com.google.firebase.database.*
+    import android.widget.TextView
+    import android.widget.Toast
+    import android.app.DatePickerDialog
+    import android.content.ContentValues.TAG
+    import android.icu.util.Calendar
+    import android.util.Log
+    import android.view.View
+    import androidx.constraintlayout.widget.ConstraintLayout
+    import com.google.firebase.auth.FirebaseAuth
+    import com.google.firebase.database.DatabaseError
+    import com.google.firebase.database.FirebaseDatabase
+    import com.google.firebase.database.ValueEventListener
+    import java.io.Serializable
+    import java.text.SimpleDateFormat
+    import java.util.Locale
+    import com.google.firebase.firestore.FirebaseFirestore
 
-class EditProfileScreen : AppCompatActivity() {
-    private lateinit var dobTextView: TextView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile_screen)
-
-        // Find the views
-        val gotoallQuotes = findViewById<ImageButton>(R.id.imageButton)
-        val gotoHomeScreen = findViewById<ImageButton>(R.id.imageButton2)
-        val gotoProfileScreen = findViewById<ImageButton>(R.id.imageButton3)
-        val genderSpinner: Spinner = findViewById(R.id.genderSpinner)
-        val dobButton: Button = findViewById(R.id.dobButton)
-        val submitButton: Button = findViewById(R.id.submitButton)
-        dobTextView = findViewById(R.id.dobTextView)
-
-        // Set click listeners
-        gotoallQuotes.setOnClickListener {
-            startActivity(Intent(applicationContext, AllQuotesScreen::class.java))
-        }
-        gotoHomeScreen.setOnClickListener {
-            startActivity(Intent(applicationContext, HomeScreen::class.java))
-        }
-        gotoProfileScreen.setOnClickListener {
-            startActivity(Intent(applicationContext, ProfileScreen::class.java))
-        }
-        dobButton.setOnClickListener {
-            showDatePickerDialog()
-        }
-        submitButton.setOnClickListener {
-            saveProfileDetails()
-        }
-
-        // Initialize gender spinner
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.genders_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            genderSpinner.adapter = adapter
-        }
-    }
-
-    private fun showDatePickerDialog() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+    class EditProfileScreen : AppCompatActivity() {
+        private lateinit var dobTextView: TextView
+        private lateinit var firstNameEditText: EditText
+        private lateinit var lastNameEditText: EditText
+        private lateinit var genderSpinner: Spinner
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
 
-        val datePickerDialog = DatePickerDialog(
-            this,
-            { _, selectedYear, selectedMonth, selectedDay ->
-                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
-                dobTextView.text = selectedDate
-            },
-            year,
-            month,
-            day
-        )
-        // Set max date to today to prevent selecting future dates
-        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
-        datePickerDialog.show()
-    }
+            setContentView(R.layout.activity_edit_profile_screen)
+            firstNameEditText = findViewById(R.id.firstNameEditText)
+            lastNameEditText = findViewById(R.id.lastNameEditText)
+            genderSpinner = findViewById(R.id.genderSpinner)
+            dobTextView = findViewById(R.id.dobTextView)
 
-    private fun saveProfileDetails() {
-        val firstName = findViewById<EditText>(R.id.firstNameEditText).text.toString()
-        val lastName = findViewById<EditText>(R.id.lastNameEditText).text.toString()
-        val gender = findViewById<Spinner>(R.id.genderSpinner).selectedItem.toString()
-        val dob = dobTextView.text.toString()
 
-        // Check if any field is empty
-        if (firstName.isEmpty() || lastName.isEmpty() || dob.isEmpty()) {
-            // Show warning message
-            findViewById<TextView>(R.id.warningTextView).visibility = View.VISIBLE
-            return
 
-        }
+            // Find the views
+            val gotoallQuotes = findViewById<ImageButton>(R.id.imageButton)
+            val gotoHomeScreen = findViewById<ImageButton>(R.id.imageButton2)
+            val gotoProfileScreen = findViewById<ImageButton>(R.id.imageButton3)
+            val genderSpinner: Spinner = findViewById(R.id.genderSpinner)
+            val dobButton: Button = findViewById(R.id.dobButton)
+            val submitButton: Button = findViewById(R.id.submitButton)
+            dobTextView = findViewById(R.id.dobTextView)
 
-        // Save details to Firebase Realtime Database
-        val database = FirebaseDatabase.getInstance("https://quotesapp-5a307-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("users")
-        val user = HashMap<String, String>()
-        user["firstName"] = firstName
-        user["lastName"] = lastName
-        user["gender"] = gender
-        user["dob"] = dob
-
-        database.push().setValue(user)
-            .addOnSuccessListener {
-                Toast.makeText(this, "Saved successfully", Toast.LENGTH_SHORT).show()
+            // Set click listeners
+            gotoallQuotes.setOnClickListener {
+                startActivity(Intent(applicationContext, AllQuotesScreen::class.java))
             }
-            .addOnFailureListener {
-                Toast.makeText(this, "Failed to Save", Toast.LENGTH_SHORT).show()
+            gotoHomeScreen.setOnClickListener {
+                startActivity(Intent(applicationContext, HomeScreen::class.java))
             }
+            gotoProfileScreen.setOnClickListener {
+                startActivity(Intent(applicationContext, ProfileScreen::class.java))
+            }
+            dobButton.setOnClickListener {
+                showDatePickerDialog()
+            }
+            submitButton.setOnClickListener {
+                saveProfileDetails()
+            }
+
+            // Initialize gender spinner
+            ArrayAdapter.createFromResource(
+                this,
+                R.array.genders_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                genderSpinner.adapter = adapter
+            }
+        }
+
+        private fun showDatePickerDialog() {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                    dobTextView.text = selectedDate
+                },
+                year,
+                month,
+                day
+            )
+            // Set max date to today to prevent selecting future dates
+            datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+            datePickerDialog.show()
+        }
+
+
+        private fun saveProfileDetails() {
+            val firstName = firstNameEditText.text.toString()
+            val lastName = lastNameEditText.text.toString()
+            val gender = genderSpinner.selectedItem.toString()
+            val dob = dobTextView.text.toString()
+
+            // Check if any field is empty
+            if (firstName.isEmpty() || lastName.isEmpty() || dob.isEmpty()) {
+                // Show warning message
+                findViewById<TextView>(R.id.warningTextView).visibility = View.VISIBLE
+                return
+            }
+
+            val currentTime = Calendar.getInstance().time
+            val sdf = SimpleDateFormat("MMMM yyyy, h a", Locale.getDefault())
+            val formattedTime = sdf.format(currentTime)
+
+            val db = FirebaseFirestore.getInstance()
+            val user = HashMap<String, Any>()
+            user["firstName"] = firstName
+            user["lastName"] = lastName
+            user["gender"] = gender
+            user["dob"] = dob
+            user["updatedDate"] = formattedTime
+
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    Toast.makeText(this, "Saved successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                    Toast.makeText(this, "Failed to Save", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+
+
     }
-}
+
+
+    data class User(
+        var firstName: String? = "",
+        var lastName: String? = "",
+        var gender: String? = "",
+        var dob: String? = "",
+        var updatedDate: String? = ""
+    )
+
+
--- app/src/main/java/com/example/quotesapp/ProfileScreen.kt
+++ app/src/main/java/com/example/quotesapp/ProfileScreen.kt
@@ -1,18 +1,44 @@
 package com.example.quotesapp
 
 import android.content.Intent
+import android.health.connect.datatypes.units.Length
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import com.google.firebase.auth.FirebaseAuth
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 
 class ProfileScreen : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var firstNameTextView: TextView
+    private lateinit var lastNameTextView: TextView
+    private lateinit var genderTextView: TextView
+    private lateinit var dobTextView: TextView
+    private lateinit var updatedDateTextView: TextView
+
+    private lateinit var database: DatabaseReference
+    private lateinit var currentUser: FirebaseUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_screen)
         auth = FirebaseAuth.getInstance()
+
+        // Initialize Firebase Database reference
+        database =
+            FirebaseDatabase.getInstance("https://quotesapp-5a307-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
+        currentUser = auth.currentUser!!
+        fetchUserData()
+
+        firstNameTextView = findViewById(R.id.textView2)
+        lastNameTextView = findViewById(R.id.textView3)
+        genderTextView = findViewById(R.id.textView4)
+        dobTextView = findViewById(R.id.textView5)
+        updatedDateTextView = findViewById(R.id.textView6)
 
 
         val gotoallQuotes = findViewById<ImageButton>(R.id.imageButton)
@@ -25,7 +51,7 @@ class ProfileScreen : AppCompatActivity() {
             val intent = Intent(applicationContext, HomeScreen::class.java)
             startActivity(intent)
         }
-        val  gotoEditScreen = findViewById<ImageButton>(R.id.imageButton4)
+        val gotoEditScreen = findViewById<ImageButton>(R.id.imageButton4)
         gotoEditScreen.setOnClickListener {
             val intent = Intent(applicationContext, EditProfileScreen::class.java)
             startActivity(intent)
@@ -40,11 +66,34 @@ class ProfileScreen : AppCompatActivity() {
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
             }
-        } else {
-            // User is not logged in, do something else if needed
+
         }
+    }
+
+    private fun fetchUserData() {
+        val userId = currentUser.uid
+        database.child("users").child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(User::class.java)
+                user?.let {
+                    firstNameTextView.text = it.firstName
+                    lastNameTextView.text = it.lastName
+                    genderTextView.text = it.gender
+                    dobTextView.text = it.dob
+                    updatedDateTextView.text = it.updatedDate
+                    // Show success message
+                    Toast.makeText(applicationContext, "Data fetched successfully", Toast.LENGTH_SHORT).show()
+                }
+            }
 
 
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+                Toast.makeText(applicationContext, "Failed to fetch data: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
 
+        })
     }
 }
+
+
